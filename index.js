dc459b9c13f271d91e2bdc299b87f9b95e8ece0e
abishr <adhambishr@gmail.com>
2018-03-06 15:14:17 -0800

    working through async use of inquirer

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const controllers = require("./controllers/controllers");
 const inquirer = require("inquirer");
+let queries = {
+  maxResults: 3
+}
 
-
-// controllers.searchLibrary({     topic: 'war' })
+// controllers.searchLibrary({ topic: 'war' })
 
 inquirer
   .prompt([
   {
     type: "input",
-    message: "Welcome to the Google Books API. \n What books would you like to search for today? ",
+    message: "Welcome to the Google Books API. What books would you like to search for today? ",
     name: "searchQuery"
   }
 ])
   .then(inquirerResponse => {
-    let queries = {
-      maxResults: 3
-    }
+
     if (!inquirerResponse.searchQuery) {
       throw new Error('Please Enter A Proper Response')
     }
-    queries.q= inquirerResponse.searchQuery
-    controllers.searchLibrary(queries)
+    queries.q = inquirerResponse.searchQuery
+    //controllers.searchLibrary(queries)
 
-  }).then(() =>{
-    controllers.reorderCSV()
   })
+  .then(() => {
+    optionsMenu(queries)
+  }).then(() =>{
+    console.log('program has reached its end')
+  })
+
+function optionsMenu(listObj) {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Would you like to add any parameters",
+      choices: [
+        'None', 'Maximize Results'
+      ],
+      name: "parameterChoice"
+    }
+  ]).then(res => {
+    if (res.parameterChoice == 'Maximize Results') {
+      maxResults(listObj)
+    }
+  })
+}
+
+function maxResults(listObj) {
+  inquirer
+    .prompt([
+    {
+      type: "input",
+      message: "What is the number of results you would like? max 40",
+      name: "maxResults"
+    }
+  ])
+    .then(res => {
+      listObj['maxResults'] = res.maxResults
+      console.log(listObj)
+    })
+}
